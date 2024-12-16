f0c4bc4e1562542450a9525e56fa3bdcd3812f2e
sloupok2227 <slowy144@yandex.ru>
2024-12-16 17:42:45 +0300

    Add missing files

--- sleep_tracker/sleep_tracker/settings.py
+++ sleep_tracker/sleep_tracker/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = ["*"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Application definition
 
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sleep_tracker.urls'
